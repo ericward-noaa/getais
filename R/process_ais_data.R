7a1ff884a7289f2aaef05dfac1a00928c13afbeb
eric-ward <eric.ward@noaa.gov>
2017-09-05 13:10:17 -0700

    redid function with new speed calculations

--- R/process_ais_data.R
+++ R/process_ais_data.R
@@ -1,20 +1,37 @@
-library(lubridate)
-library(dplyr)
-library(PBSmapping)
-library(fields)
-
-process_ais_data = function(aisdata) {
+process_ais_data = function(aisdata, min_from_hr = 7.5, time_zone = "GMT") {
   # The whale data should be in GMT, so we need to convert
-  # the AIS data to GMT
-  aisdata$BaseDateTime = as.POSIXct(aisdata$BaseDateTime, tz="GMT")
+  # the AIS data to GMT?
 
-  # remove minutes not within 5 min of hr
+  aisdata$BaseDateTime = as.POSIXct(aisdata$BaseDateTime, tz = time_zone)
+
+  aisdata = arrange(aisdata, MMSI, BaseDateTime) %>%
+    group_by(MMSI) %>%
+    mutate(diff_time = c(NA, diff(BaseDateTime)),
+      lon_lag = c(coords.x1[-1],NA),
+      lat_lag = c(coords.x2[-1],NA),
+      diff_dist = rdist.earth.vec(cbind(coords.x1,coords.x2),
+        cbind(lon_lag, lat_lag), miles=FALSE),
+      speed_kmhr = 60*diff_dist/diff_time)
+
+  # remove minutes not within 7.5 min of hr.
   g = aisdata %>% ungroup %>%
-    mutate(mint = minute(as_datetime(aisdata$BaseDateTime, tz="GMT"))) %>%
-    filter(mint >= 55 | mint < 5) %>%
+    mutate(mint = minute(as_datetime(aisdata$BaseDateTime, tz=time_zone)),
+      hourt = hour(as_datetime(aisdata$BaseDateTime, tz=time_zone)),
+      dayt = day(as_datetime(aisdata$BaseDateTime, tz=time_zone)),
+      montht = month(as_datetime(aisdata$BaseDateTime, tz=time_zone)),
+      yeart = year(as_datetime(aisdata$BaseDateTime, tz=time_zone))) %>%
+    filter(mint >= (60-min_from_hr) | mint < (min_from_hr)) %>%
+    mutate(time_id = paste(yeart,montht,dayt,hourt, sep="-")) %>%
+    select(-hourt, -dayt, -montht, -yeart) %>%
+    group_by(MMSI, time_id) %>%
+    mutate(speed_kmhr2 = sum(diff_dist, na.rm=T)/sum(diff_time, na.rm=T)) %>%
     select(-COG, -Heading, -ROT, -ReceiverType, -ReceiverID, -keep, -mint) %>%
     rename(X = coords.x1) %>%
-    rename(Y = coords.x2)
+    rename(Y = coords.x2) %>%
+    filter(is.finite(speed_kmhr2)) %>%
+    filter(is.finite(speed_kmhr)) %>%
+    filter(!is.na(speed_kmhr2)) %>%
+    filter(!is.na(speed_kmhr))
 
   # convert to km
   g$PID = 1
