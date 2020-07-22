e2f3784c0a75b6d69f6f733773cbc46b62969f86
Olin Feist <feisto@wwu.edu>
2020-07-22 13:20:59 -0700

    Fixed Compiler warnings, removed sorting, added code to find indexs of needed columns and fixed bugs

--- src/parseCSV.cpp
+++ src/parseCSV.cpp
@@ -6,258 +6,231 @@
 #include <vector>
 #include <iomanip>
 #include <Rcpp.h>
-#include <chrono>
 
 using namespace std;
-using namespace std::chrono;
 using namespace Rcpp;
 
-//currentrowval class
-class currentrowval{
-public:
-  int mmsi;
-  std::string basedatetime;
-  float lat;
-  float longitude;
-  float sog;
-  float cog;
-  int heading;
-  std::string vesselname;
-  std::string imo;
-  std::string callsign;
-  std::string vesseltype;
-  std::string status;
-  std::string length;
-  std::string width;
-  std::string draft;
-  std::string cargofile;
 
-  currentrowval(int  mmsi1, std::string basedatetime1,float lat1, float longitude1, float sog1, float cog1,
-    int heading1, std::string vesselname1, std::string imo1, std::string callsign1, std::string vesseltype1, std::string status1, std::string length1, std::string width1,std::string draft1,std::string cargofile1){
-    mmsi=mmsi1;
-    basedatetime=basedatetime1;
-    lat=lat1;
-    longitude=longitude1;
-    sog=sog1;
-    cog=cog1;
-    heading=heading1;
-    vesselname=vesselname1;
-    imo=imo1;
-    callsign=callsign1;
-    vesseltype=vesseltype1;
-    status=status1;
-    length=length1;
-    width=width1;
-    draft=draft1;
-    cargofile=cargofile1;
-  }
+//global variables
+    //column indexs of csv file
+    int mmsiindex;
+    int basedatetimeindex;
+    int latindex; 
+    int longitudeindex;
+    int sogindex; 
+    int cogindex;
+    int headingindex;
+    int vesselnameindex;
+    int imoindex; 
+    int callsignindex;
+    int vesseltypeindex;
+    int statusindex;
+    int lengthindex; 
+    int widthindex;
+    int draftindex;
+    int cargofileindex;
+
+
+//-------------------------------------------------------
+//csvrow class
+class csvrow{
+    public:
+    int mmsi;
+    string basedatetime;
+    float lat; 
+    float longitude;
+    float sog; 
+    float cog;
+    int heading;
+    string vesselname;
+    string imo; 
+    string callsign;
+    string vesseltype;
+    string status;
+    string length; 
+    string width;
+    string draft;
+    string cargofile;
+
+    csvrow(int  mmsi1, string basedatetime1,float lat1, float longitude1, float sog1, float cog1,
+    int heading1, string vesselname1, string imo1, string callsign1, string vesseltype1, string status1, string length1, string width1,string draft1,string cargofile1){
+        mmsi=mmsi1;
+        basedatetime=basedatetime1;
+        lat=lat1;
+        longitude=longitude1;
+        sog=sog1;
+        cog=cog1;
+        heading=heading1;
+        vesselname=vesselname1;
+        imo=imo1;
+        callsign=callsign1;
+        vesseltype=vesseltype1;
+        status=status1;
+        length=length1;
+        width=width1;
+        draft=draft1;
+        cargofile=cargofile1;        
+    }
 
 };
-//vector of currentrowval objects to store csvtables
-vector<currentrowval> csvtable;
+
 //compare function to sort by time
-bool comparetime(const currentrowval& r1, const currentrowval& r2){
-  int day1=stoi((r1.basedatetime).substr(8,2));
-  int day2=stoi((r2.basedatetime).substr(8,2));
-  int hour1=stoi((r1.basedatetime).substr(11,2));
-  int hour2=stoi((r2.basedatetime).substr(11,2));
-  int min1=stoi((r1.basedatetime).substr(14,2));
-  int min2=stoi((r2.basedatetime).substr(14,2));
-  int sec1=stoi((r1.basedatetime).substr(17,2));
-  int sec2=stoi((r2.basedatetime).substr(17,2));
+bool comparetime(const csvrow& r1, const csvrow& r2){
 
-  // All cases when true should be returned
-  if (day1 < day2){
-    return true;
-  }else if (day1 == day2 && hour1 < hour2){
-    return true;
-  }else if (day1 == day2 && hour1 == hour2 && min1 < min2){
-    return true;
-  }else if(day1 == day2 && hour1 == hour2 && min1 == min2 && sec1<sec2){
-    return true;
-  }
+    int day1=stoi((r1.basedatetime).substr(8,2));
+    int day2=stoi((r2.basedatetime).substr(8,2));
+    int hour1=stoi((r1.basedatetime).substr(11,2));
+    int hour2=stoi((r2.basedatetime).substr(11,2));
+    int min1=stoi((r1.basedatetime).substr(14,2));
+    int min2=stoi((r2.basedatetime).substr(14,2));
+    int sec1=stoi((r1.basedatetime).substr(17,2));
+    int sec2=stoi((r2.basedatetime).substr(17,2));
 
-
-  return false;
+    // All cases when true should be returned 
+    if (day1 < day2){ 
+        return true; 
+    }else if (day1 == day2 && hour1 < hour2){ 
+        return true; 
+    }else if (day1 == day2 && hour1 == hour2 && min1 < min2){ 
+        return true;
+    }else if(day1 == day2 && hour1 == hour2 && min1 == min2 && sec1<sec2){
+        return true; 
+    }
+            
+            
+    return false; 
 }
 
 //determine if number is close to given interval
 bool findclose(int a,int interval){
-  int temp=0;
-  while(temp<=a+1){
-    if(a-temp==-1){
+    int temp=0;
+    while(temp<=a+1){
+        if(a-temp==-1){
+           
+            return true;
+        }else{
+            temp+=interval;
+        }
 
-      return true;
-    }else{
-      temp+=interval;
     }
+    return false;
+}
+//-------------------------------------------------------
 
-  }
-  return false;
+
+
+//look through csv headers and find the index of each the date and status
+void findcolindexs(string line){
+    vector<string> v;
+    stringstream ss(line); 
+    while (ss.good()) { 
+        string substr; 
+        getline(ss, substr, ',');   
+        v.push_back(substr); 
+    }
+    int index=0;
+    for(string x:v){
+        
+        if(x=="BaseDateTime"){
+            basedatetimeindex=index;
+       
+        }else if(x=="Status"){
+            statusindex=index;
+        }
+
+        index++;
+
+    }
 }
 
+//method to parse csv
+
 // [[Rcpp::export]]
-void parsefile(std::string filename,int interval,int variance){
+void parsefile(string filename,int interval,int variance){
+    
 
-  //import csv file to "file"
-  ifstream file(filename.c_str());
+    //import csv file to "file"
+    ifstream file(filename.c_str());
 
-  //skip first line of csv file (Skip Headers)
-  std::string line;
-  getline(file, line);
+    //skip first line of csv file (Skip Headers)
+    string line;
+    getline(file, line);
 
-  //save headers for later
-  std::string headers=line;
+    //save headers for later
+    string headers=line;
+    findcolindexs(line);
 
-  //test if file was loaded
-  if(line.empty()){
-    std::cout<<"Error: file not loaded"<<endl;
-    return;
-  }else{
-    std::cout<<"File Loaded..."<<endl;
-    std::cout<<"Running..."<<endl;
-  }
-
-
-
-
-
-
-
-
-  //object to hold currentrowval data
-  currentrowval* temp;
-
-
-  //vector to hold currentrowval data
-  vector<std::string> v;
-
-  //auto start = high_resolution_clock::now();
-
-  //loop through csv file
-  while (getline(file, line)){
-
-
-    //clear vector
-    v.clear();
-
-    //get current line value and add it to sting line then convert to string stream
-    stringstream ss(line);
-
-    //convert line into set of strings, store in vector v
-    while (ss.good()) {
-      std::string substr;
-      getline(ss, substr, ',');
-
-      v.push_back(substr);
+    //test if file was loaded
+    if(line.empty()){
+        cout<<"Error: file not loaded"<<endl;
+        return;
     }
 
 
-    //only add by 5 min itervals and ignore certain statuses
-
-    //convert min and seconds to ints
-    int tempint=stoi(v[1].substr(14,2));
-    int tempint2=stoi(v[1].substr(17,2));
+    //object to hold csvrow data
+ 
 
 
+    //vector to hold csvrow data
+    vector<string> v;
 
-    //filter by type
-    if(v[11]!=("at anchor")&&(v[11]!=("moored"))&&(v[11]!=("aground"))){
+    //open file and write headers to it
+    ofstream file_stream;
+    file_stream.open ("soundparse.csv");
+    file_stream<<headers<<endl;
 
-      //5 min interval
-      if(tempint%interval==0){
-        if(tempint2<=variance){
-          temp=new currentrowval(stoi(v[0]),v[1],stof(v[2]),stof(v[3]),stof(v[4]),stof(v[5]),stoi(v[6]),v[7],v[8],v[9],v[10],v[11],v[12],v[13],v[14],v[15]);
-          csvtable.push_back(*temp);
-        }
-      }else if(findclose(tempint,interval)){
-        if(tempint2>=60-variance){
-          temp=new currentrowval(stoi(v[0]),v[1],stof(v[2]),stof(v[3]),stof(v[4]),stof(v[5]),stoi(v[6]),v[7],v[8],v[9],v[10],v[11],v[12],v[13],v[14],v[15]);
-          csvtable.push_back(*temp);
 
+    //loop through csv file
+    while (getline(file, line)){
+      
+        
+        //clear vector
+        v.clear();
+
+        //get current line value and add it to sting line then convert to string stream
+        stringstream ss(line); 
+
+        //convert line into set of strings, store in vector v
+        while (ss.good()) { 
+            string substr; 
+            getline(ss, substr, ','); 
+          
+            v.push_back(substr); 
         }
 
-      }
+
+        //only add by 5 min itervals and ignore certain statuses
+        
+        //convert min and seconds to ints
+        int tempint=stoi(v[basedatetimeindex].substr(14,2));
+        int tempint2=stoi(v[basedatetimeindex].substr(17,2));
+
+        //filter by type
+        if(v[statusindex]!=("at anchor")&&(v[statusindex]!=("moored"))&&(v[statusindex]!=("aground"))){
+            
+            //5 min interval
+            if(tempint%interval==0){
+               if(tempint2<=variance){
+                    file_stream<<line<<"\n";
+               }
+            }else if(findclose(tempint,interval)){
+                if(tempint2>=60-variance){
+                    file_stream<<line<<"\n";
+                }
+
+            }
+        }
+ 
     }
 
-  }
-
-  cout<<"Parsed by "<< interval <<" min intervals"<<endl;
-
-  //sort csvtable by mmsi
-  //sort(csvtable.begin(), csvtable.end(), [](const currentrowval& lhs, const currentrowval& rhs) {
-  //  return lhs.mmsi < rhs.mmsi;
-  //});
-
-  //cout<<"sorted by boat id"<<endl;
-  //--------------------
 
 
-  //sort boats by time --------
-  int startindex=0;
-  int endindex=0;
-  int boatid=csvtable[0].mmsi;
+    
 
-  for(int i=0;i<csvtable.size();i++){
+    file_stream.close();
+    //------------------------- 
 
-    if(boatid!=csvtable[i].mmsi||i==csvtable.size()-1){
-
-
-      sort(csvtable.begin()+startindex, csvtable.begin()+endindex, comparetime);
-
-      boatid=csvtable[i].mmsi;
-      startindex=i;
-      endindex=i;
-    }else{
-      endindex++;
-    }
-  }
-  //cout<<"sorted by time"<<endl;
-  //-------------------------
-
-  //write to csv file ----------
-  ofstream file_stream;
-
-  file_stream.open ("soundparse.csv");
-
-  //write headers to file
-  file_stream<<headers<<endl;
-  //loop through csvtable and write to file
-  for(int i=0;i<csvtable.size();i++){
-    currentrowval currentcurrentrowval=csvtable[i];
-    file_stream<<currentcurrentrowval.mmsi<<",";
-    file_stream<<currentcurrentrowval.basedatetime<<",";
-    file_stream<<currentcurrentrowval.lat<<",";
-    file_stream<<currentcurrentrowval.longitude<<",";
-    file_stream<<currentcurrentrowval.sog<<",";
-    file_stream<<currentcurrentrowval.cog<<",";
-    file_stream<<currentcurrentrowval.heading<<",";
-    file_stream<<currentcurrentrowval.vesselname<<",";
-    file_stream<<currentcurrentrowval.imo<<",";
-    file_stream<<currentcurrentrowval.callsign<<",";
-    file_stream<<currentcurrentrowval.vesseltype<<",";
-    file_stream<<currentcurrentrowval.status<<",";
-    file_stream<<currentcurrentrowval.length<<",";
-    file_stream<<currentcurrentrowval.width<<",";
-    file_stream<<currentcurrentrowval.draft<<",";
-    file_stream<<currentcurrentrowval.cargofile<<endl;
-
-
-  }
-
-  file_stream.close();
-  //-------------------------
-
-  //get time of run
-  //auto stop = high_resolution_clock::now();
-  //auto duration = duration_cast<seconds>(stop - start);
-  //float time=duration.count()/60;
-  //cout<<"Done."<<endl;
-  //cout<<setprecision(2)<<"Elapsed Time: "<<(duration.count()-(duration.count()%60))/60<<":"<<duration.count()%60<<" minutes,seconds" << endl;
-  //cout<<"number of records: "<<csvtable.size()-1<<endl;
-
-
-
+    
 }
 
 
