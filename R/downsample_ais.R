60824d4f6167715ca700b7ef0a7c6357310c0d18
eric-ward <eric.ward@noaa.gov>
2020-09-15 13:53:58 -0700

    commit owen's changes

--- R/downsample_ais.R
+++ R/downsample_ais.R
@@ -101,11 +101,12 @@ for(i in 1:nrow(df)) {
       #if(df$year!="2017") {
         fname = paste0("AIS_ASCII_by_UTM_Month/",df$year[i],"/AIS_",df$year[i],"_",char_month,"_Zone",char_zone,".csv")
       #}
-      dat = read.csv(file = fname,
-        stringsAsFactors = FALSE)
+      dat = read.csv(file = fname,stringsAsFactors = FALSE)
     }
 
+    # remove temporary files
     unlink("temp.zip")
+
     # remove gdb file according to user input
     if(delete_gdb) unlink(fname)
     # filter out status codes
@@ -130,37 +131,54 @@ for(i in 1:nrow(df)) {
 
     if(raw == FALSE) {
       # round the time to nearest minute
-      dat$BaseDateTime_round = lubridate::round_date(dat$BaseDateTime, "minute")
+      # dat$BaseDateTime_round = lubridate::round_date(dat$BaseDateTime, "minute")
+      #
+      # dat$minutes = lubridate::minute(dat$BaseDateTime_round) + lubridate::second(dat$BaseDateTime)/60
+      # seq_min = seq(0, 60, by = every_minutes)
+      # seq_min[1] = -1 # catch against 00:00:00 throwing error
+      # dat$time_chunk = as.numeric(cut(dat$minutes, seq_min))
+      # # calculate difference between time stamp and minutes rounded down and minutes rounded up
+      # dat$diff_1 = abs(dat$minutes - seq_min[dat$time_chunk])
+      # dat$diff_2 = abs(dat$minutes - seq_min[dat$time_chunk+1])
+      # # indicator for whether time stamp is closer to lower (diff_1) or upper bound (diff_2)
+      # dat$time_1 = ifelse(dat$diff_1 < dat$diff_2, 1, 0)
+      # dat$min_timediff = dat$diff_1 * dat$time_1 + (1-dat$time_1)*dat$diff_2  # calculate the smallest of the 2 values
+      # # the time_chunk here is really the interval that the timestamp falls into, and the dat$interval is which of the
+      # # breaks the time stamp is closest to
+      # dat$interval = dat$time_chunk * dat$time_1 + (1-dat$time_1)* (dat$time_chunk+1) # similarly assign the chunk --
+      #
+      # # also need to catch case where redundant
+      # # or which of the breaks this is closest to
+      # dat = select(dat, -BaseDateTime_round, -minutes, -time_chunk, -diff_1, -diff_2, -time_1)
+      #
+      # # create interval: month: day to group_by on -- this is month:day:hour:interval
+      # chunk_fac = as.factor(paste0(lubridate::month(dat$BaseDateTime),":",
+      #   lubridate::day(dat$BaseDateTime),":",lubridate::hour(dat$BaseDateTime),":",dat$interval))
+      # dat$chunk = as.numeric(chunk_fac)
+      # dat$BaseDateTime = as.character(dat$BaseDateTime) # needed to keep dplyr from throwing error
+      #
+      # dat = dplyr::group_by(dat, MMSI, chunk) %>%
+      # dplyr::mutate(mintime = ifelse(min_timediff == min(min_timediff), 1, 0)) %>%
+      # dplyr::filter(mintime == 1) %>%
+      # ungroup %>%
+      # dplyr::select(-mintime, -chunk, -keep)
 
-      dat$minutes = lubridate::minute(dat$BaseDateTime_round) + lubridate::second(dat$BaseDateTime)/60
-      seq_min = seq(0, 60, by = every_minutes)
-      seq_min[1] = -1 # catch against 00:00:00 throwing error
-      dat$time_chunk = as.numeric(cut(dat$minutes, seq_min))
-      # calculate difference between time stamp and minutes rounded down and minutes rounded up
-      dat$diff_1 = abs(dat$minutes - seq_min[dat$time_chunk])
-      dat$diff_2 = abs(dat$minutes - seq_min[dat$time_chunk+1])
-      # indicator for whether time stamp is closer to lower (diff_1) or upper bound (diff_2)
-      dat$time_1 = ifelse(dat$diff_1 < dat$diff_2, 1, 0)
-      dat$min_timediff = dat$diff_1 * dat$time_1 + (1-dat$time_1)*dat$diff_2  # calculate the smallest of the 2 values
-      # the time_chunk here is really the interval that the timestamp falls into, and the dat$interval is which of the
-      # breaks the time stamp is closest to
-      dat$interval = dat$time_chunk * dat$time_1 + (1-dat$time_1)* (dat$time_chunk+1) # similarly assign the chunk --
+      periodkey <- tibble(start=seq(floor_date(min(dat$BaseDateTime), unit='hours'),
+        ceiling_date(max(dat$BaseDateTime), unit='hours'),
+        by=paste(every_minutes,"min"))) %>%
+        # round down such that the intervals are on the hour (e.g. every 30 minutes even, 00:00, 00:30, 01:00 etc.)
+        # then assign an idx (i.e., a chunk number)
+        dplyr::mutate(start=floor_date(start,paste(every_minutes,"min")),idx=row_number())
 
-      # also need to catch case where redundant
-      # or which of the breaks this is closest to
-      dat = select(dat, -BaseDateTime_round, -minutes, -time_chunk, -diff_1, -diff_2, -time_1)
-
-      # create interval: month: day to group_by on -- this is month:day:hour:interval
-      chunk_fac = as.factor(paste0(lubridate::month(dat$BaseDateTime),":",
-        lubridate::day(dat$BaseDateTime),":",lubridate::hour(dat$BaseDateTime),":",dat$interval))
-      dat$chunk = as.numeric(chunk_fac)
-      dat$BaseDateTime = as.character(dat$BaseDateTime) # needed to keep dplyr from throwing error
-
-      dat = dplyr::group_by(dat, MMSI, chunk) %>%
-      dplyr::mutate(mintime = ifelse(min_timediff == min(min_timediff), 1, 0)) %>%
-      dplyr::filter(mintime == 1) %>%
-      ungroup %>%
-      dplyr::select(-mintime, -chunk, -keep)
+      # round each observation down to nearest "every_minutes", then join the chunk key
+      dat <- dat %>% mutate(flr_time=floor_date(BaseDateTime,paste(every_minutes,"min"))) %>%
+        dplyr::left_join(periodkey,by=c("flr_time"="start")) %>%
+        # group by vessel and time index, and choose the first observation(i.e., the first observation in each "every_minutes" chunk)
+        dplyr::group_by(MMSI,idx) %>%
+        dplyr::arrange(BaseDateTime) %>%
+        dplyr::slice(1) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-idx)
     }
 
     if(nrow(dat) > 0) {
